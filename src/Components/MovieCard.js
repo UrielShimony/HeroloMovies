8a0293115d68b7e174e0bccdcece60a1ef71d0ca
uriel shimony <>
2018-10-13 10:56:30 +0300

    Design movie card

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,35 +1,20 @@
 import React, {Component} from 'react';
 import '../Styles/MovieCard.css'
 import Icon from 'antd/lib/icon'
-import ReactStars from 'react-stars'
 import APILayer from '../APIUtils/APILayer'
+import {Rate} from "antd";
 
 
 const imageUrlPrefix = 'https://image.tmdb.org/t/p/w500';
 
 class MovieCard extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    componentDidMount() {
-    }
-
-    genreTostring(genreList) {
-        console.log(genreList.map((id)=>APILayer.getGenreName(id)));
-        return APILayer.getGenreName(genreList[0])
-    }
 
     render() {
         const {movie, deleteCallBack, editCallBack} = this.props;
-        console.log('movie', movie);
-        console.log(this.props);
         return (
             <div className='card'>
                 <div className='imageContainer'>
-                    <img width={'100%'}
-                         height={'100%'}
-                         src={`${imageUrlPrefix}${movie.poster_path}`}/>
+                    <img src={`${imageUrlPrefix}${movie.poster_path}`}/>
                 </div>
                 <div className='cardDataContainer'>
                     <Icon className='deleteIcon' type="close-circle" theme="outlined"
@@ -39,12 +24,10 @@ class MovieCard extends Component {
                             <p className='movieTitle'>{movie.title}</p>
                         </div>
                         <div className='field'>
-                            <ReactStars className='reactStars'
-                                        count={5}
-                                        size={20}
-                                        edit={false}
-                                        value={movie.vote_average / 2}
-                                        color2={'#ffd700'}/>
+                            <Rate className='reactStars'
+                                  allowHalf
+                                   disabled
+                                   value={Math.round(movie.vote_average)/2} />
                             {movie.vote_average}
                         </div>
                         <div className='field'>
@@ -53,7 +36,7 @@ class MovieCard extends Component {
                         </div>
                         <div className='field'>
                             <p className='boldField'>Genre: </p>
-                            <p>{this.genreTostring(movie.genre_ids).toString()}</p>
+                            <p className='genreDetails'>{this.genreTostring(movie.genre_ids).toString()}</p>
                         </div>
                     </div>
                     <div className='cardEditField'>
@@ -65,6 +48,10 @@ class MovieCard extends Component {
                 </div>
             </div>
         );
+    }
+    genreTostring(genreList) {
+        return (genreList.map((id)=>APILayer.getGenreName(id)))
+            .toString().replace(/,/g, ', ');
     }
 }
 
